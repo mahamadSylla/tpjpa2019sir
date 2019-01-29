3e7e209d6ad7e2b776dd341007bf99807ed9664f
Mahamadou SYLLA <mahamdsylla5@gmail.com>
2019-01-29 16:49:47 +0100

    update files

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,33 +1,62 @@
 package jpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
 public class JpaTest {
+
+	private EntityManager manager;
+
+	public JpaTest(EntityManager manager) {
+		this.manager = manager;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		// EntityManagerFactory factory =
+		// Persistence.createEntityManagerFactory("tpjpa");
 		EntityManager manager = EntityManagerHelper.getEntityManager();
+		JpaTest test = new JpaTest(manager);
 		EntityTransaction tx = manager.getTransaction();
 		tx.begin();
 
-
 		try {
-
+			test.createEmployees();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		tx.commit();
 
-
 		manager.close();
 		EntityManagerHelper.closeEntityManagerFactory();
-		//		factory.close();
+		// factory.close();
 	}
 
+	private void createEmployees() {
+		int numOfEmployees = manager.createQuery("Select a From Employee a", Employee.class).getResultList().size();
+		if (numOfEmployees == 0) {
+			Department department = new Department("java");
+			manager.persist(department);
+
+			manager.persist(new Employee("Jakab Gipsz", department));
+			manager.persist(new Employee("Captain Nemo", department));
+
+		}
+	}
+
+	private void listEmployees() {
+		List<Employee> resultList = manager.createQuery("Select a From Employee a", Employee.class).getResultList();
+		System.out.println("num of employess:" + resultList.size());
+		for (Employee next : resultList) {
+			System.out.println("next employee: " + next);
+		}
+	}
 
 }
